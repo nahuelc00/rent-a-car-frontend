0bc68dabd278258065d490bdaf59b0be15bd4204
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-02 18:37:21 -0300

    add url in link of detail

--- src/ui/components/CarCard.jsx
+++ src/ui/components/CarCard.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function CarCard({ brand, passengers, kms }) {
+function CarCard({
+  id, brand, passengers, kms,
+}) {
   return (
     <div className="card-car box has-background-grey-lighter is-flex is-flex-direction-column is-align-items-center">
       <h1 className="subtitle mb-3 has-text-weight-bold">{ brand }</h1>
       <p className="mb-1">{`${passengers} passengers`}</p>
       <p className="mb-2">{`${kms} kms`}</p>
-      <Link href>Detail</Link>
+      <Link to={`detail/${id}`}>Detail</Link>
       <div>
         <Link href className="mr-1">Edit</Link>
         <Link href>Delete</Link>
@@ -18,6 +20,7 @@ function CarCard({ brand, passengers, kms }) {
 }
 
 CarCard.propTypes = {
+  id: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
   passengers: PropTypes.number.isRequired,
   kms: PropTypes.number.isRequired,
