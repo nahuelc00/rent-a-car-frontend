5389990fe6e39c1995fa1ec91d9c3268943f20b4
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-02 18:01:30 -0300

    create PageExit component

---
+++ src/ui/components/PageExit.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+function PageExit({ exitRoute }) {
+  return (
+    <Link to={exitRoute}>
+      <img src="/images/cross-close.svg" alt="cross-close" />
+    </Link>
+  );
+}
+
+PageExit.propTypes = {
+  exitRoute: PropTypes.string.isRequired,
+};
+
+export { PageExit };
