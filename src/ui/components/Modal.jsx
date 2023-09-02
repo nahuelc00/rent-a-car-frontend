3781896f9f3b98d9d5698d69f6bc7c1db336df85
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-02 18:01:13 -0300

    create Modal component

---
+++ src/ui/components/Modal.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { PageExit } from './PageExit';
+
+function Modal({ title, exitRoute }) {
+  return (
+    <div className="modal is-active">
+      <div className="modal-background" />
+      <div className="modal-card">
+        <header className="modal-card-head is-flex is-justify-content-end">
+          <PageExit exitRoute={exitRoute} />
+        </header>
+        <section className="modal-card-body">
+          <h2 className="title">{title}</h2>
+        </section>
+        <footer className="modal-card-foot" />
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  title: PropTypes.string.isRequired,
+  exitRoute: PropTypes.string.isRequired,
+};
+
+export { Modal };
