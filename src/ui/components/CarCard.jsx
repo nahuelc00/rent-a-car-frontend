4bcb3fd1ca702446123fa8cf6c94515d0889841b
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-02 17:50:32 -0300

    create Loader and CarCard components

---
+++ src/ui/components/CarCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function CarCard({ brand, passengers, kms }) {
+  return (
+    <div className="card-car box has-background-grey-lighter is-flex is-flex-direction-column is-align-items-center">
+      <h1 className="subtitle mb-3 has-text-weight-bold">{ brand }</h1>
+      <p className="mb-1">{`${passengers} passengers`}</p>
+      <p className="mb-2">{`${kms} kms`}</p>
+      <Link href>Detail</Link>
+      <div>
+        <Link href className="mr-1">Edit</Link>
+        <Link href>Delete</Link>
+      </div>
+    </div>
+  );
+}
+
+CarCard.propTypes = {
+  brand: PropTypes.string.isRequired,
+  passengers: PropTypes.number.isRequired,
+  kms: PropTypes.number.isRequired,
+};
+
+export { CarCard };
---
+++ src/ui/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Loader() {
+  return (
+    <div className="loader is-size-2" />
+  );
+}
+
+export { Loader };
