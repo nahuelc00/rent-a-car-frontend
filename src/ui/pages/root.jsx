e911bc30f34ec0592c2db4486246c36f3234bc1d
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-02 17:52:31 -0300

    add loader and carCard components in root page

--- src/ui/pages/root.jsx
+++ src/ui/pages/root.jsx
@@ -3,6 +3,8 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import { useGetCars } from '../../hooks/useGetCars';
+import { Loader } from '../components/Loader';
+import { CarCard } from '../components/CarCard';
 
 function Root() {
   const { cars, isLoading } = useGetCars();
@@ -10,7 +12,7 @@ function Root() {
   if (isLoading) {
     return (
       <div className="is-flex is-justify-content-center">
-        <div className="loader is-size-2" />
+        <Loader />
       </div>
     );
   }
@@ -20,20 +22,17 @@ function Root() {
       <h1 className="title mb-4 has-text-centered">Rent a car</h1>
       <Link to="form/add" className="button is-info mb-6 has-text-weight-medium">Add car</Link>
       <div className="root-page__container-cards-car">
-        { cars.map((car) => (
-          <div key={car.id} className="root-page__card-car box has-background-grey-lighter is-flex is-flex-direction-column is-align-items-center">
-            <h1 className="subtitle mb-3 has-text-weight-bold">{ car.brand }</h1>
-            <p className="mb-1">{`${car.passengers} passengers`}</p>
-            <p className="mb-2">{ `${car.kms} kms` }</p>
-            <Link href>Detail</Link>
-            <div>
-              <Link href className="mr-1">Edit</Link>
-              <Link href>Delete</Link>
-            </div>
-          </div>
+        { cars.map(({
+          id, brand, passengers, kms,
+        }) => (
+          <CarCard
+            key={id}
+            brand={brand}
+            passengers={passengers}
+            kms={kms}
+          />
         )) }
       </div>
-
     </>
   );
 }
