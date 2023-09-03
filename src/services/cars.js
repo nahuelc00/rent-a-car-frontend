f8b74766738baf3393cbf2335392f1e4a917334a
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-03 14:26:15 -0300

    add functionality to get car and save car in service

--- src/services/cars.js
+++ src/services/cars.js
@@ -4,4 +4,18 @@ function getCars() {
   return fetch(`${API_HOST}/`).then((res) => res.json());
 }
 
-export { getCars };
+function getCar(id) {
+  return fetch(`${API_HOST}/car/${id}`).then((res) => res.json());
+}
+
+function saveCar(car) {
+  return fetch(`${API_HOST}/cars`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(car),
+  });
+}
+
+export { getCars, saveCar, getCar };
