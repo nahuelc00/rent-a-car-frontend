6b14021d388b88a8c1341a1aafe8414ecefec8b8
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-05 22:17:19 -0300

    add a custom hook to handle delete of car

---
+++ src/hooks/useHandleDeleteCar.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { deleteCar } from '../services/cars';
+
+function useHandleDeleteCar() {
+  const [endOfDelete, setEndOfDelete] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleDeleteCar(id) {
+    setIsLoading(true);
+    deleteCar(id).then(() => {
+      setIsLoading(false);
+      setEndOfDelete(true);
+    });
+  }
+
+  return { handleDeleteCar, isLoading, endOfDelete };
+}
+
+export { useHandleDeleteCar };
