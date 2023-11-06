b32d3b81f9f7abf4ca22b0a4555cd41f096ce90d
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-06 16:30:27 -0300

    Update style buttons of header

--- src/ui/components/Header.jsx
+++ src/ui/components/Header.jsx
@@ -37,10 +37,10 @@ function Header() {
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
-                <Link to="/register" className="button is-primary">
+                <Link to="/register" className="button is-primary is-medium">
                   <strong>Sign up</strong>
                 </Link>
-                <Link to="/login" className="button is-light">
+                <Link to="/login" className="button is-light is-medium">
                   Log in
                 </Link>
               </div>
