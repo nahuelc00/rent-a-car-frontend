455eff1b44549a69d39af48588501d5bc840dd84
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-09 17:34:04 -0300

    Change register data of register form

--- src/ui/pages/auth/registerForm.jsx
+++ src/ui/pages/auth/registerForm.jsx
@@ -10,13 +10,9 @@ import { PageExit } from '../../components/PageExit';
 function validateForm(userData) {
   const {
     firstname, lastname, email, password,
-    documentType, documentNumber, nationality,
-    phone, dateOfBirth, address,
   } = userData;
 
-  const hasEmptyValues = firstname === '' || lastname === '' || email === '' || password === ''
-    || documentType === '' || documentNumber === '' || nationality === '' || phone === ''
-    || dateOfBirth === '' || address === '';
+  const hasEmptyValues = firstname === '' || lastname === '' || email === '' || password === '';
 
   if (hasEmptyValues) return false;
 
@@ -46,12 +42,6 @@ function RegisterForm() {
       lastname: '',
       email: '',
       password: '',
-      documentType: '',
-      documentNumber: '',
-      nationality: '',
-      phone: '',
-      dateOfBirth: '',
-      address: '',
     },
     onSubmit: async (userData) => {
       const isFormValid = validateForm(userData);
@@ -77,7 +67,7 @@ function RegisterForm() {
     <>
       <PageExit exitRoute="/" />
       <h1 className="title mt-5">Registration</h1>
-      <form style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '50px' }} onSubmit={formik.handleSubmit}>
+      <form style={{ maxWidth: '700px' }} onSubmit={formik.handleSubmit}>
 
         <div className="field">
           <label className="label is-size-4">Firstname</label>
@@ -119,119 +109,6 @@ function RegisterForm() {
             </span>
           </div>
           {!formik.values.lastname && <p className="help is-danger is-size-5">This lastname is invalid</p>}
-        </div>
-
-        <div className="field">
-          <label className="label is-size-4">Document type</label>
-          <div className="select is-medium">
-            <select defaultValue="default" name="documentType" onChange={formik.handleChange}>
-              <option value="default" disabled>Select dropdown</option>
-              <option value="DNI">DNI</option>
-            </select>
-          </div>
-          {!formik.values.documentType && <p className="help is-danger is-size-5">This document is invalid</p> }
-
-        </div>
-
-        <div className="field">
-          <label className="label is-size-4">Document number</label>
-          <div className="control has-icons-left has-icons-right">
-            <input
-              className={formik.values.documentNumber ? 'input is-success is-medium' : 'input is-danger is-medium'}
-              type="number"
-              name="documentNumber"
-              placeholder="Document number"
-              onChange={formik.handleChange}
-              value={formik.values.documentNumber}
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-user" />
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-check" />
-            </span>
-          </div>
-          {!formik.values.documentNumber && <p className="help is-danger is-size-5">This document number is invalid</p> }
-        </div>
-
-        <div className="field">
-          <label className="label is-size-4">Nationality</label>
-          <div className="control has-icons-left has-icons-right">
-            <input
-              className={formik.values.nationality ? 'input is-success is-medium' : 'input is-danger is-medium'}
-              name="nationality"
-              type="text"
-              placeholder="Nationality"
-              onChange={formik.handleChange}
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-user" />
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-check" />
-            </span>
-          </div>
-          {!formik.values.nationality && <p className="help is-danger is-size-5">This nationality is invalid</p>}
-        </div>
-
-        <div className="field">
-          <label className="label is-size-4">Phone</label>
-          <div className="control has-icons-left has-icons-right">
-            <input
-              className={formik.values.phone ? 'input is-success is-medium' : 'input is-danger is-medium'}
-              name="phone"
-              type="text"
-              placeholder="Phone"
-              onChange={formik.handleChange}
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-user" />
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-check" />
-            </span>
-          </div>
-          {!formik.values.phone && <p className="help is-danger is-size-5">This phone is invalid</p>}
-        </div>
-
-        <div className="field">
-          <label className="label is-size-4">Address</label>
-          <div className="control has-icons-left has-icons-right">
-            <input
-              className={formik.values.address ? 'input is-success is-medium' : 'input is-danger is-medium'}
-              name="address"
-              type="text"
-              placeholder="Address"
-              onChange={formik.handleChange}
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-user" />
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-check" />
-            </span>
-          </div>
-          {!formik.values.address && <p className="help is-danger is-size-5">This address is invalid</p>}
-        </div>
-
-        <div className="field">
-          <label className="label is-size-4">Date of birth</label>
-          <div className="control has-icons-left has-icons-right">
-            <input
-              className={formik.values.dateOfBirth ? 'input is-success is-medium' : 'input is-danger is-medium'}
-              name="dateOfBirth"
-              type="date"
-              placeholder="Date of birth"
-              onChange={formik.handleChange}
-            />
-            <span className="icon is-small is-left">
-              <i className="fas fa-user" />
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-check" />
-            </span>
-          </div>
-          {!formik.values.dateOfBirth && <p className="help is-danger is-size-5">This date is invalid</p>}
         </div>
 
         <div className="field">
