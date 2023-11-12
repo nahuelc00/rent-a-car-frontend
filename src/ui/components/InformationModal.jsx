33c98e668222899e828a2f8387b3c28081090e18
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-12 14:05:37 -0300

    Update modal information component to handle when exiting the modal

--- src/ui/components/InformationModal.jsx
+++ src/ui/components/InformationModal.jsx
@@ -1,15 +1,19 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { PageExit } from './PageExit';
 
-function InformationModal({ title, exitRoute }) {
+function InformationModal({ title, exitRoute, handleExitRoute }) {
   return (
     <div className="modal is-active">
       <div className="modal-background" />
       <div className="modal-card">
         <header className="modal-card-head is-flex is-justify-content-end">
-          <PageExit exitRoute={exitRoute} />
+          <div onClick={handleExitRoute}>
+            <PageExit exitRoute={exitRoute} />
+          </div>
         </header>
         <section className="modal-card-body">
           <h2 className="title">{title}</h2>
@@ -20,9 +24,15 @@ function InformationModal({ title, exitRoute }) {
   );
 }
 
+InformationModal.defaultProps = {
+  exitRoute: '',
+  handleExitRoute: () => {},
+};
+
 InformationModal.propTypes = {
   title: PropTypes.string.isRequired,
-  exitRoute: PropTypes.string.isRequired,
+  exitRoute: PropTypes.string,
+  handleExitRoute: PropTypes.func,
 };
 
 export { InformationModal };
