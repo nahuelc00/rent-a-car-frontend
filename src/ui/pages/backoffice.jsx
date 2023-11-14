5232a5f57a4dbc05ab398b264096be915659d0a1
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-14 17:49:30 -0300

    Add validation when is user admin in backoffice page

--- src/ui/pages/backoffice.jsx
+++ src/ui/pages/backoffice.jsx
@@ -3,28 +3,53 @@ import { ClientForm } from '../components/ClientForm';
 import { PageExit } from '../components/PageExit';
 import { getUser } from '../../services/users';
 import { getAccessToken } from '../../utilities';
+import { Loader } from '../components/Loader';
 
 function Backoffice() {
   const [userFirstname, setUserFirstname] = useState('');
+  const [isUserAdmin, setIsUserAdmin] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const accessToken = getAccessToken();
     getUser(accessToken).then((user) => {
-      setUserFirstname(user.firstname);
+      const isAdmin = user.roles.includes('admin');
+      if (isAdmin) {
+        setUserFirstname(user.firstname);
+        setIsUserAdmin(true);
+        setIsLoading(false);
+      }
+
+      if (!isAdmin) {
+        setIsLoading(false);
+      }
     });
   }, []);
-  return (
-    <>
-      <header className="mb-5">
-        <PageExit exitRoute="/" />
-      </header>
-      <main>
-        <h1 className="title is-size-2">{`Hi ${userFirstname}!`}</h1>
-        <h2 className="title">Register a client here:</h2>
-        <ClientForm />
-      </main>
-    </>
-  );
+
+  if (isLoading) {
+    return (
+      <div className="is-flex is-justify-content-center">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (isUserAdmin) {
+    return (
+      <>
+        <header className="mb-5">
+          <PageExit exitRoute="/" />
+        </header>
+        <main>
+          <h1 className="title is-size-2">{`Hi ${userFirstname}!`}</h1>
+          <h2 className="title">Register a client here:</h2>
+          <ClientForm />
+        </main>
+      </>
+    );
+  }
+
+  return <h1 className="title is-size-1">Not authorized</h1>;
 }
 
 export { Backoffice };
