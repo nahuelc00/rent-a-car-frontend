65f0349594fc5bff4fb0d5adc5133218963d7a19
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-14 14:48:16 -0300

    Update styles and add firstname of user in backoffice page

--- src/ui/pages/backoffice.jsx
+++ src/ui/pages/backoffice.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ClientForm } from '../components/ClientForm';
 import { PageExit } from '../components/PageExit';
+import { getUser } from '../../services/users';
+import { getAccessToken } from '../../utilities';
 
 function Backoffice() {
+  const [userFirstname, setUserFirstname] = useState('');
+
+  useEffect(() => {
+    const accessToken = getAccessToken();
+    getUser(accessToken).then((user) => {
+      setUserFirstname(user.firstname);
+    });
+  }, []);
   return (
     <>
-      <header>
+      <header className="mb-5">
         <PageExit exitRoute="/" />
       </header>
       <main>
-        <h1 className="title">Hi user!</h1>
+        <h1 className="title is-size-2">{`Hi ${userFirstname}!`}</h1>
         <h2 className="title">Register a client here:</h2>
         <ClientForm />
       </main>
