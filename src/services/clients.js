68a705ce4d53dc906dec1a8f6707a3cb74842373
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-14 14:02:31 -0300

    Add constant API_HOST in clients services

--- src/services/clients.js
+++ src/services/clients.js
@@ -1,5 +1,7 @@
+const API_HOST = import.meta.env.VITE_API_HOST;
+
 function registerClient(clientData) {
-  return fetch(`${import.meta.env.VITE_API_HOST}/client/register`, {
+  return fetch(`${API_HOST}/client/register`, {
     headers: {
       'content-type': 'application/json',
     },
