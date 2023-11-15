770722fcc0848d73e8c48a2954e7a98a59bae17c
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-15 19:43:46 -0300

    Move car components to a folder

--- src/ui/components/cars/CarCard.jsx
+++ src/ui/components/cars/CarCard.jsx
@@ -6,14 +6,14 @@ function CarCard({
   id, brand, passengers, kms,
 }) {
   return (
-    <div className="card-car box has-background-grey-lighter is-flex is-flex-direction-column is-align-items-center">
+    <div className="car-card box has-background-grey-lighter is-flex is-flex-direction-column is-align-items-center">
       <h1 className="subtitle mb-3 has-text-weight-bold">{ brand }</h1>
       <p className="mb-1">{`${passengers} passengers`}</p>
       <p className="mb-2">{`${kms} kms`}</p>
-      <Link to={`car/detail/${id}`}>Detail</Link>
+      <Link to={`/car/detail/${id}`}>Detail</Link>
       <div>
-        <Link to={`car/edit/${id}`} className="mr-1">Edit</Link>
-        <Link to={`car/delete/${id}`}>Delete</Link>
+        <Link to={`/car/edit/${id}`} className="mr-1">Edit</Link>
+        <Link to={`/car/delete/${id}`}>Delete</Link>
       </div>
     </div>
   );
