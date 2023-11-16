3d43f13af0040db245ad175c8d24bda84897a77b
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-16 17:01:26 -0300

    Update layout and add validation when is user logged in profile page

--- src/ui/pages/profile.jsx
+++ src/ui/pages/profile.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { getAccessToken } from '../../utilities';
+import { Header } from '../components/Header';
 
 function Profile() {
-  return (
-    <>
-      <h1 className="title">Profile</h1>
-      <Link className="is-size-5" to="/logout">Log out</Link>
-    </>
-  );
+  const accessToken = getAccessToken();
+  const isUserLogged = accessToken !== undefined;
+
+  if (isUserLogged) {
+    return (
+      <>
+        <Header />
+        <main className="mt-5">
+          <h1 className="title">Profile</h1>
+          <Link className="is-size-5" to="/logout">Log out</Link>
+        </main>
+
+      </>
+    );
+  }
+
+  return <h1 className="title is-size-1">Not authorized</h1>;
 }
 
 export { Profile };
