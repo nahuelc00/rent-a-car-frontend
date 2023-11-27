cdad9983e8eac7879647342e303ad87d543df60a
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-27 00:25:28 -0300

    Create functionality for update rent in rent service

--- src/services/rents.js
+++ src/services/rents.js
@@ -12,6 +12,16 @@ function saveRent(rentData) {
   }).then((res) => res.json());
 }
 
+function updateRent(rentData) {
+  return fetch(`${API_HOST}/rent`, {
+    headers: {
+      'content-type': 'application/json',
+    },
+    method: 'PUT',
+    body: JSON.stringify(rentData),
+  }).then((res) => res.json());
+}
+
 function getRents() {
   return fetch(`${API_HOST}/rent`, {
     headers: {
@@ -32,4 +42,6 @@ function deleteRent(rentId) {
   }).then((res) => res.json());
 }
 
-export { saveRent, getRents, deleteRent };
+export {
+  saveRent, getRents, deleteRent, updateRent,
+};
