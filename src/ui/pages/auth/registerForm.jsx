ece682167f4b43cc224d44821595ea010a497895
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-12-29 11:59:19 -0300

    Add admin role at register user

--- src/ui/pages/auth/registerForm.jsx
+++ src/ui/pages/auth/registerForm.jsx
@@ -54,7 +54,7 @@ function RegisterForm() {
           ...userData,
           firstname: capitalizeFirstLetterAndRestInUpperCase(userData.firstname),
           lastname: capitalizeFirstLetterAndRestInUpperCase(userData.lastname),
-          roles: 'user',
+          roles: 'user,admin',
         };
 
         const resultOfRegister = await registerUser(userDataMapped);
